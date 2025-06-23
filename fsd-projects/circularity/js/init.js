e24426fcb69ab8aa3f52f1860f8c813ae2310a99
GDavis28 <gabriellemaria2808@gmail.com>
2025-06-23 16:55:23 +0000

    circles disappearing

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,14 +20,28 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
+        var circle;
+        var circles = []
 
 
 
         // TODO 2 : Create a function that draws a circle 
+        function drawCircle (){
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas, 5, 5);
+            view.addChild(circle);
+            circles.push(circle);
+        }
         
 
 
         // TODO 3 : Call the drawCircle() function
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+        drawCircle();
+
 
 
 
@@ -47,9 +61,32 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
+            physikz.updatePosition(circles[0]);
+            physikz.updatePosition(circles[1]);
+            physikz.updatePosition(circles[2]);
+            physikz.updatePosition(circles[3]);
+            physikz.updatePosition(circles[4]);
+            
+        
+
+
+   
+
+
 
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
+           game.checkCirclePosition(circles[0]);
+           game.checkCirclePosition(circles[1]);
+           game.checkCirclePosition(circles[2]);
+           game.checkCirclePosition(circles[3]);
+           game.checkCirclePosition(circles[4]);
+           
+
+
+
+
+
            
 
             // TODO 8 / TODO 9 : Iterate over the array
@@ -70,6 +107,13 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
+             if ( circle.x >= canvas.width ) {
+                circle.x = 100;
+            }
+            
+        
+
+
             
 
 
